c6e3d40978dd2c6b483b9251758872c160cb39ae
Sudeep Juvekar <sjuvekar@gmail.com>
2015-10-07 21:56:35 -0400

    modified

--- src/base_model.py
+++ src/base_model.py
@@ -5,30 +5,27 @@ from feature_extractor import FeatureExtractor
 from cached_feature_extractor import CachedFeatureExtractor
 
 class BaseModel(object):
-    
+
+    """
+    This is the Base class for all models
+    There are two ways of training any of the derived models:
+    1) Online: override the _fit_internal() method that trains on a batch
+    2) Complete: override the fit() method that trains on entire training set
+    """
     def __init__(self, cached_features=True):
         self.model = None
         self.classes = numpy.array([0, 1])
         self.cached_features = cached_features
-        if cached_features:
-            self.feature_extractor = CachedFeatureExtractor("working/models/train_batches")
-            self.test_feature_extractor = CachedFeatureExtractor("working/models/test_batches")
-        else:
-            self.feature_extractor = FeatureExtractor("data/train.csv")
-            self.test_feature_extractor = FeatureExtractor("data/test.csv")
+        self.feature_extractor = CachedFeatureExtractor("working/models/train_batches")
+        self.test_feature_extractor = CachedFeatureExtractor("working/models/test_batches")
+        
 
     def fit(self):
         print "   Fitting ", self.__class__.__name__, "..."
         counter = 0
         for (X_train, y_train) in self.feature_extractor.nextBatch():
             self._fit_internal(X_train, y_train)
-            if not self.cached_features:
-                dump_file = "working/models/train_batches/{}.pickle".format(counter)
-                print "Dumping to {}".format(dump_file)
-                self.feature_extractor.dumpBatch((X_train, y_train), dump_file)
-                counter += 1
-
-
+            
     def _fit_internal(self, X_train, y_train):
         self.model.partial_fit(X_train, y_train, classes=self.classes)
 
@@ -40,11 +37,6 @@ class BaseModel(object):
 
         for (X_test, y_test_ignored) in self.test_feature_extractor.nextBatch():
             y_ans = numpy.append(y_ans, self._predict_internal(X_test))
-            if not self.cached_features:
-                dump_file = "working/models/test_batches/{}.pickle".format(counter)
-                print "Dumping to {}".format(dump_file)
-                self.test_feature_extractor.dumpBatch((X_test, y_test_ignored), dump_file)
-                counter += 1
         return y_ans
    
 
--- src/train.py
+++ src/train.py
@@ -3,30 +3,17 @@ import numpy
 import pandas
 import sys
 
-from feature_extractor import FeatureExtractor
-from cached_feature_extractor import CachedFeatureExtractor
 
-from sgd_model import SGDModel
-from perceptron_model import PerceptronModel
 from naive_bayes_model import NaiveBayesModel
-from passive_aggressive_model import PassiveAggressiveModel
-from gboost_model import GBoostModel
 from keras_model import KerasModel
-from lstm_model import LSTMModel
-from convnet_model import ConvnetModel
 
 if __name__ == "__main__":
     
     cached = True
 
-    models = { "sgd_model": "SGDModel", 
-               "perceptron_model": "PerceptronModel",
-               "naive_bayes_model": "NaiveBayesModel",
-               "passive_aggressive_model": "PassiveAggressiveModel",
-               "gboost_model": "GBoostModel",
-               "keras_model": "KerasModel",
-               "convnet_model": "ConvnetModel"
-    }
+    models = {"naive_bayes_model": "NaiveBayesModel",
+              "keras_model": "KerasModel",
+              }
 
     model_name = sys.argv[1]
     print "Creating", model_name, ": ",
